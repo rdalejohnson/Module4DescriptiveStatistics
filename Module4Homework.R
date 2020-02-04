676e069e30f9d182b7e8cf6c69d72a04cdc460ca
Johnson <rdjohnson@ad.hs.uab.edu>
2020-02-04 15:03:11 -0600

    Completed mapping to numeric values to strings

--- Module4Homework.R
+++ Module4Homework.R
@@ -14,5 +14,24 @@ lab=read.csv("Binge_Drinking.csv")
 
 
 # change "1/2" to "Male/Female"
-lab$gender = gsub(1, "Male", lab$gender)
-lab$gender = gsub(2, "Female",lab$gender)
+lab$gender = gsub(1, "Male",   lab$gender)
+lab$gender = gsub(2, "Female", lab$gender)
+
+# change college class numbers to words
+lab$class = gsub(1, "Freshman", lab$class)
+lab$class = gsub(2, "Sophomore", lab$class)
+lab$class = gsub(3, "Junior", lab$class)
+lab$class = gsub(4, "Senior", lab$class)
+lab$class = gsub(5, "5th year or Higher", lab$class)
+
+lab$fratsoro = gsub(1, "Frat/Sorority Member", lab$fratsoro)
+lab$fratsoro = gsub(2, "Not a Frat/Sorority Member", lab$fratsoro)
+
+--# of drinking binges over the last two weeks
+lab$drinks5 = gsub(1, "No binge", lab$drinks5)
+lab$drinks5 = gsub(2, "One Binge", lab$drinks5)
+lab$drinks5 = gsub(3, "Two Binges", lab$drinks5)
+lab$drinks5 = gsub(4, "Three to Five Binges", lab$drinks5)
+lab$drinks5 = gsub(5, "Six to Nine Binges", lab$drinks5)
+lab$drinks5 = gsub(6, "Ten or More Binges", lab$drinks5)
+
